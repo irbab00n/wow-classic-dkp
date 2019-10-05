b5da9eecb64f8f3e147d0f3ef2e3b9910e96aa92
Tommy Cosby <cosbyts@gmail.com>
2019-10-04 22:42:05 -0700

    Tuck the playableClassesIndex method success console logs inside the verbose option

--- src/controllers/blizzard/classes.ts
+++ src/controllers/blizzard/classes.ts
@@ -5,7 +5,6 @@ import { PlaceholderObject } from '../../Interfaces/internal/PlaceholderObject';
 import { getToken } from './getToken';
 import { createAuthHeaders } from './shared';
 
-
 // ===============
 // DEFAULT OPTIONS
 // ===============
@@ -16,7 +15,8 @@ const _getCharacterDefaultOptions = {
 // ==============
 // ERROR MESSAGES
 // ==============
-const ERROR_PLAYABLE_CLASS_INDEX_FETCH_FAILED = 'playableClassesIndex --- Something went wrong while attempting to retrieve the playable class index from the WoW Classic API';
+const ERROR_PLAYABLE_CLASS_INDEX_FETCH_FAILED =
+  'playableClassesIndex --- Something went wrong while attempting to retrieve the playable class index from the WoW Classic API';
 
 // ==============
 // MODULE CONFIGS
@@ -28,15 +28,12 @@ const blizzard_api = 'api.blizzard.com';
 const namespace = 'static-classic-us';
 const locale = 'en_US';
 
-'https://us.api.blizzard.com/data/wow/playable-class/7?namespace=static-classic-us&locale=en_US&access_token=USfPqP2h82Nd8qBvKAatHEUC4DYsoA3ZJc'
-
-
 // ==============
 // MODULE STORAGE
 // ==============
 
 // Module storage for the Item Classes Index
-// Item Classes are generic groups of items; 
+// Item Classes are generic groups of items;
 var playableClassesIndex: null | PlaceholderObject = null;
 
 // =======
@@ -46,34 +43,50 @@ var playableClassesIndex: null | PlaceholderObject = null;
 // Retrieves the playable classes index from the module storage
 // If the module storage hasn't been populated with data from the World of Warcraft API
 // It will be retireved and stored within the module
-export const getPlayableClassesIndex = async ({ verbose }: BasicFunctionOptions = _getCharacterDefaultOptions) => {
+export const getPlayableClassesIndex = async ({
+  verbose,
+}: BasicFunctionOptions = _getCharacterDefaultOptions) => {
   const playableClassesIndexEndpoint = 'data/wow/playable-class/index';
   if (verbose) {
-    console.log('playableClassesIndex --- checking if module playableClassesIndex storage is empty...\n');
+    console.log(
+      'playableClassesIndex --- checking if module playableClassesIndex storage is empty...\n'
+    );
   }
 
   if (playableClassesIndex) {
     if (verbose) {
-      console.log('playableClassesIndex --- playableClassesIndex has been retrieved from the module storage...\n');
+      console.log(
+        'playableClassesIndex --- playableClassesIndex has been retrieved from the module storage...\n'
+      );
     }
     return playableClassesIndex;
   } else {
     if (verbose) {
-      console.log('playableClassesIndex --- playableClassesIndex is empty; attempting to retrieve list...\n');
+      console.log(
+        'playableClassesIndex --- playableClassesIndex is empty; attempting to retrieve list...\n'
+      );
     }
-    let accessToken = await getToken({ verbose: true });
+    let accessToken = await getToken();
     let requestURL = `${protocol}://${region}.${blizzard_api}/${playableClassesIndexEndpoint}?namespace=${namespace}&locale=${locale}&access_token=${accessToken}`;
-    let requestConfig: PlaceholderObject = createAuthHeaders(accessToken); 
+    let requestConfig: PlaceholderObject = createAuthHeaders(accessToken);
     let retrievedIndex = await axios.get(requestURL, requestConfig);
     if (retrievedIndex) {
-      console.log('playableClassesIndex --- playableClassesIndex retrieved!\n');
-      console.log('playableClassesIndex --- ', retrievedIndex.data, '\n');
+      if (verbose) {
+        console.log(
+          'playableClassesIndex --- playableClassesIndex retrieved!\n'
+        );
+        console.log('playableClassesIndex --- ', retrievedIndex.data, '\n');
+      }
       playableClassesIndex = retrievedIndex.data;
       return retrievedIndex;
     } else {
-      console.error('retrieved Index was not the expected result: ', retrievedIndex);
+      console.error(
+        'retrieved Index was not the expected result: ',
+        retrievedIndex
+      );
       throw new Error(ERROR_PLAYABLE_CLASS_INDEX_FETCH_FAILED);
     }
+    // ! Keeping this here for now as a reference if we need to move from the async/await pattern
     // return axios.get(requestURL, requestConfig)
     //   .then((results: any) => {
     //     console.log('playableClassesIndex --- playableClassesIndex retrieved!\n');
