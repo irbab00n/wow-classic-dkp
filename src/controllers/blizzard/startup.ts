d37df61cd8426000a9594ede67c3ec18c26fa075
Thomas Cosby <cosbyts@gmail.com>
2019-10-08 18:15:33 -0700

    Print normalized JSON list for items classes and their subclasses
    Cleanup blizzard startup script

--- src/controllers/blizzard/startup.ts
+++ src/controllers/blizzard/startup.ts
@@ -1,3 +1,29 @@
 import fs from 'fs';
+import { getToken } from './getToken';
 import { getItemClass, getItem } from './items';
+import itemClasses from '../../lib/item-classes.json';
+import Axios from 'axios';
 import path from 'path';
+
+const parsedItemClasses = JSON.parse(JSON.stringify(itemClasses));
+const locale = 'en_US';
+
+// Get item classes
+// // const retrieveItemSubClassesForClasses = () => {
+// getToken().then(async (accessToken: string) => {
+//   // console.log('parsedItemClasses: ', parsedItemClasses, '\n');
+//   let mappedClasses = parsedItemClasses.map((itemClass: any) => {
+//     return new Promise((resolve, reject) => {
+//       return Axios.get(`${itemClass.key.href}&locale=${locale}&access_token=${accessToken}`).then((results: any) => {
+//         // console.log('results of getting the item class: ', results);
+//         let mappedResult = Object.assign(itemClass, results.data);
+//         if (mappedResult.hasOwnProperty('key')) delete mappedResult.key;
+//         resolve(mappedResult);
+//       })
+//     });
+//   });
+//   let combinedResults = await Promise.all(mappedClasses)
+//   // fs.writeFileSync(path.resolve(__dirname, '../../lib/item-classes-subclasses.json'), JSON.stringify(combinedResults));
+//   return combinedResults;
+// });
+// // };
